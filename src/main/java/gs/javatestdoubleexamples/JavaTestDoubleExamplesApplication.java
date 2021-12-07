25188a1afe66c70ce9bc0ec5eef4405a65ed8fb8
Gavin Shaw <gshaw+github@pivotal.io>
2021-12-07 17:54:40 -0500

    Inital commit to set up a spring boot app

---
+++ src/main/java/gs/javatestdoubleexamples/JavaTestDoubleExamplesApplication.java
@@ -0,0 +1,13 @@
+package gs.javatestdoubleexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestDoubleExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestDoubleExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gs/javatestdoubleexamples/JavaTestDoubleExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package gs.javatestdoubleexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestDoubleExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
